4358927d730bbca1fb0670131b3781370577706b
mjoliveir <32xxthunderxx32@gmail.com>
2025-09-26 15:44:23 -0300

    Validação das informações do metodo put

--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -19,12 +19,33 @@ public class Endereco {
     private String cidade;
     private String uf;
 
-    public Endereco(DadosEndereco dados){
+    public Endereco(DadosEndereco dados) {
         this.logradouro = dados.logradouro();
         this.uf = dados.uf();
         this.bairro = dados.bairro();
         this.cep = dados.cep();
         this.cidade = dados.cidade();
         this.numero = dados.numero();
+    }
+
+    public void atualizarInformacoes(DadosEndereco dados) {
+        if (dados.logradouro() != null) {
+            this.logradouro = dados.logradouro();
+        }
+        if (dados.uf() != null) {
+            this.uf = dados.uf();
+        }
+        if (dados.bairro() != null) {
+            this.bairro = dados.bairro();
+        }
+        if (dados.cep() != null) {
+            this.cep = dados.cep();
+        }
+        if (dados.cidade() != null) {
+            this.cidade = dados.cidade();
+        }
+        if (dados.numero() != null) {
+             this.numero = dados.numero();
+        }
     }
 }
