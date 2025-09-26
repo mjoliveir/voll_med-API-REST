fe7dbabdd77d16c2a3dab5d398f87697c9a2db39
mjoliveir <32xxthunderxx32@gmail.com>
2025-09-26 15:42:00 -0300

    configuração de getters alem de novos métodos como o de atualização de dados de pacientes

--- src/main/java/med/voll/api/pacientes/Paciente.java
+++ src/main/java/med/voll/api/pacientes/Paciente.java
@@ -1,13 +1,16 @@
 package med.voll.api.pacientes;
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import med.voll.api.controller.DadosListagemPaciente;
 import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Especialidade;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Table(name = "pacientes")
 @Entity(name = "Paciente")
@@ -25,15 +28,59 @@ public class Paciente {
     private String telefone;
     private String email;
     private String cpf;
+    private boolean ativo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
 
     @Embedded
     private Endereco endereco;
 
     public Paciente(DadosCadastroPaciente dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
         this.cpf = dados.cpf();
         this.endereco = new Endereco(dados.endereco());
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void excluir() {
+    this.ativo = false;
+    }
+
+    public void atualizarDados(@RequestBody @Valid DadosAtualizacaoPaciente dados) {
+        if (dados.nome() != null){
+            this.nome = dados.nome();
+        }if (dados.telefone() != null){
+            this.telefone = dados.telefone();
+        }if (dados.email() != null){
+            this.email = dados.email();
+        }
     }
 }
